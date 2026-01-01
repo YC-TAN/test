31218880028484fefb20e8bc1cb11ca1b004e175
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-01-01 15:13:16 +1300

    Add middleware - logger and unknownEndpoint

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,16 @@
 const express = require('express')
-const cors = require('cors')
 
 const app = express()
 
-app.use(cors())
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
 app.use(express.static('dist'))
 app.use(express.json())
 
@@ -75,6 +82,12 @@ app.post('/api/notes', (request, response) => {
   
     response.json(note)
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
